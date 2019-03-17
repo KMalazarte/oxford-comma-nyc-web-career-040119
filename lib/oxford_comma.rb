e9f35387b233bfbe75b1bac07558d25361119e2d
Kevin Malazarte <kevin.malazarte@gmail.com>
2019-03-17 00:07:11 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
    array.join(" and ")
   elsif array.length == 3
     array [0..-2].join(", ") +" and " + array[-1]
-  else
-    array.join(", ")
+  #else
+    #array.join(", ")
 end
 end
